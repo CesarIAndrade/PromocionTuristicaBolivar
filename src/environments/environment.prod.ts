b83de30a079f8024e4eb075b7f450c21183a2fd1
cesar.andrade1 <cesar.andrade@nodel.co>
2021-01-05 21:52:05 -0500

    updates to deploy to heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
   production: true
 };
+
+const ip = "26.233.202.189";
+export const apiUrl = `http://${ip}/ApiTurismo/public/api/`;
+export const imagesUrl = `http://${ip}/ApiTurismo/public/DiscoImagenes/`;
